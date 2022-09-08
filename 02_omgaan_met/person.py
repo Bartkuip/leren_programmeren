04281749de205a476b4a8c66c5285e04612180ea
Bartkuip <99027534@mydavinci.nl>
2022-09-08 11:51:47 +0200

    person finished

---
+++ 02_omgaan_met/person.py
@@ -0,0 +1,13 @@
+naamInput = input("Wat is uw naam?")
+adresInput = input("Wat is uw adres?")
+postalInput = input("Wat is uw postcode?")
+locationInput = input("Wat is uw woonplaats?")
+
+print(f'''
+ ----------------------------------------------------
+|  Naam      : {naamInput}                
+|  Adres     : {adresInput}           
+|  Postcode  : {postalInput}                         
+|  Woonplaats: {locationInput}                          
+ ----------------------------------------------------
+''')
