5e6489f47b4aa94635dc0a60c29e20c21663a479
Bartkuip <99027534@mydavinci.nl>
2022-09-09 09:32:24 +0200

    pizza edited and finished

--- 02_omgaan_met/pizzaCalculator.py
+++ 02_omgaan_met/pizzaCalculator.py
@@ -1,15 +1,14 @@
 #bart korver 99027534
-pizzaSmall = float(5.00)
-pizzaMedium = float(8.00)
-pizzaBig = float(11.00)
+pizzaSmall = 5.00
+pizzaMedium = 8.00
+pizzaBig = 11.00
 
 pizzaSmallAmount = int(input("Hoeveel kleine pizza's wilt u (26 centimeter)"))
 pizzaMediumAmount = int(input("Hoeveel medium pizza's wilt u (32 centimeter)"))
 pizzaBigAmount = int(input("Hoeveel grote pizza's wilt u (36 centimeter)"))
 total = pizzaSmall * pizzaSmallAmount + pizzaMedium * pizzaMediumAmount + pizzaBig * pizzaBigAmount
 
-print('Een totaal bedrag van:', round(total, 2),
-'euro voor de', pizzaSmallAmount, 'kleine pizza(s),', pizzaMediumAmount, ' medium pizza(s) en'
+print('Een totaal bedrag van:', total,'euro voor de', pizzaSmallAmount, 'kleine pizza(s),', pizzaMediumAmount, ' medium pizza(s) en'
 , pizzaBigAmount, ' grote pizza(s)')
 
 
