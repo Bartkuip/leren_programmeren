1204b0d57350aa3725d62f074774012347487c8e
Bartkuip <99027534@mydavinci.nl>
2022-09-09 09:35:47 +0200

    inputElse finished

--- 02_omgaan_met/inputElse.py
+++ 02_omgaan_met/inputElse.py
@@ -1,11 +1,9 @@
 getalA = input('Welk getal is A?')
 getalB = input('Welk getal is B?')
 
-print(getalA, getalB)
-
 if getalA > getalB:
     print('Getal A is groter')
 elif getalA < getalB:
     print('Getal A is kleiner')
-elif getalA == getalB:
+else:
     print('Ze zijn gelijk')
