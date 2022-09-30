ca1d884abd88e3e5eb0fc827179f27aa0054c016
Bartkuip <99027534@mydavinci.nl>
2022-09-30 08:57:29 +0200

    raket while loop done

--- 03_keer_op_keer/forever-young/opdracht6.py
+++ 03_keer_op_keer/forever-young/opdracht6.py
@@ -1,17 +1,13 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 6')
-i = 6
-
 # Jouw python instructies zet je vanaf hier:
 for x in range(0, 7):
     robotArm.moveRight()
 for x in range(0, 8):
-    for x in range(7, 0, -i):
         robotArm.grab()
         robotArm.moveRight()
         robotArm.drop()
-        i -= 1
         for x in range (0, 2):
             robotArm.moveLeft()
 
---
+++ 03_keer_op_keer/raketwhile.py
@@ -0,0 +1,7 @@
+teller = 30
+
+while teller > -1:
+    print (teller)
+    teller -=1
+    if teller == -1:
+        break
