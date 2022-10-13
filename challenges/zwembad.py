95fc83f09b448cb6df2f2064730cf837d1a9e912
Bartkuip <99027534@mydavinci.nl>
2022-10-13 09:53:26 +0200

    zwembad stap 3

--- challenges/zwembad.py
+++ challenges/zwembad.py
@@ -3,8 +3,11 @@ breedte = 3
 hoogte = 1.5
 kubiekePrijsUitgraven = 25
 kubiekePrijsGrond = 32.50
+kilometerAfstand = 60
 kubiekeTotaal = lengte * breedte * hoogte
 
 print(kubiekeTotaal * kubiekePrijsUitgraven)
 print(kubiekeTotaal * kubiekePrijsGrond)
 print(f"Totale prijs is: {kubiekePrijsGrond * kubiekeTotaal + kubiekePrijsUitgraven *kubiekeTotaal} euro")
+print(f"Voorrij kosten zijn: {2.05 * kilometerAfstand + 250} euro")
+#60 kilometer afstand, 250+ 2.05 per km
