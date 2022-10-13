9fa465a0066d9cf32f50e3efd957f5f8545e7e95
Bartkuip <99027534@mydavinci.nl>
2022-10-13 09:47:43 +0200

    zwembad stap 2

--- challenges/zwembad.py
+++ challenges/zwembad.py
@@ -1,8 +1,10 @@
 lengte = 8
 breedte = 3
 hoogte = 1.5
-kubiekePrijs = 25
+kubiekePrijsUitgraven = 25
+kubiekePrijsGrond = 32.50
+kubiekeTotaal = lengte * breedte * hoogte
 
-print(lengte * breedte * hoogte * kubiekePrijs)
-
-#8 * 3* 1.5 *25
+print(kubiekeTotaal * kubiekePrijsUitgraven)
+print(kubiekeTotaal * kubiekePrijsGrond)
+print(f"Totale prijs is: {kubiekePrijsGrond * kubiekeTotaal + kubiekePrijsUitgraven *kubiekeTotaal} euro")
