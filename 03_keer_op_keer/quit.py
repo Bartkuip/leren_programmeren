f790b301cf3d24ce1e8e41ab9ee617df29bdb80e
Bartkuip <99027534@mydavinci.nl>
2022-10-13 11:39:20 +0200

    zwembad stap 5

--- 03_keer_op_keer/quit.py
+++ 03_keer_op_keer/quit.py
@@ -10,7 +10,7 @@ while True:
         quit()
     else:
         aantal +=1
-        question = input("?", aantal)
+        question = input("?")
         
     
 
--- challenges/zwembad.py
+++ challenges/zwembad.py
@@ -1,13 +1,36 @@
 lengte = float(input("Hoe lang?"))
 breedte = float(input("Hoe breed?"))
 hoogte = float(input("Hoe hoog?"))
+tegelKeuze = input("Wat voor tegels wilt u hebben, standaard, rood of kleur").lower()
+aantalTegels = (lengte * hoogte * 2) + (breedte * hoogte * 2) + (breedte * lengte)
+vierkanteMeter = lengte * breedte
+kubiekeTotaal = lengte * breedte * hoogte
 kubiekePrijsUitgraven = 25
 kubiekePrijsGrond = 32.50
+uitgraven = round(kubiekeTotaal * kubiekePrijsUitgraven, 2)
+grond = round(kubiekeTotaal * kubiekePrijsGrond, 2)
+totaalTegel = 0
 kilometerAfstand = 60
-kubiekeTotaal = lengte * breedte * hoogte
-totaalPrijs = round(kubiekePrijsGrond * kubiekeTotaal + kubiekePrijsUitgraven *kubiekeTotaal, 2)
+#alles is nu hardcoded voor 60 kilometer afstand, kan ik evt aanpassen
+#if statement voor de m3
+if tegelKeuze == "standaard" and kubiekeTotaal < 20:
+    totaalTegel =  aantalTegels * 250
+elif tegelKeuze == "standaard" and kubiekeTotaal >= 20:
+    totaalTegel =  aantalTegels * 200
+elif tegelKeuze == "rood" and kubiekeTotaal < 20:
+    totaalTegel =  aantalTegels * 250 + 25
+elif tegelKeuze == "rood" and kubiekeTotaal >= 20:
+    totaalTegel =  aantalTegels * 200 + 20
+elif tegelKeuze == "kleur" and kubiekeTotaal < 20:
+    totaalTegel =  aantalTegels * 250 + 125
+elif tegelKeuze == "kleur" and kubiekeTotaal >= 20:
+    totaalTegel = aantalTegels * 200 + 100
 
-print(round(kubiekeTotaal * kubiekePrijsUitgraven, 2))
-print(round(kubiekeTotaal * kubiekePrijsGrond, 2))
-print(f"Totale prijs is: {totaalPrijs} euro")
-print(f"Voorrij kosten zijn: {2.05 * kilometerAfstand + 250} euro")
+totaalPrijs = round(uitgraven + grond + totaalTegel + 2.05 * kilometerAfstand + 250 ,2)
+
+
+print(f"Uitgraven:                 €{uitgraven}")
+print(f"Afvoeren grond:            €{grond}")
+print(f"Voorrijkosten:             €{2.05 * kilometerAfstand + 250}")
+print(f"Beton + tegel              €{totaalTegel}")
+print(f"Totaal:                    €{totaalPrijs}")
