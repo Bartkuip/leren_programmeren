7e809478bd7c97ccb710b1cfdb6b8f028699e0bf
Bartkuip <99027534@mydavinci.nl>
2022-11-26 10:34:33 +0100

    opdr 10

---
+++ 04_lijstjes_en_samenstellingen/fruitmand/opdracht10.py
@@ -0,0 +1,7 @@
+from fruitmand import fruitmand
+from operator import itemgetter
+
+fruitmand = sorted(fruitmand , key = itemgetter('weight', 'name'), reverse=True)
+
+for fruit in fruitmand: 
+    print(fruit['weight'], fruit['name'])
