0280f4e6ba7259b8e7a0dd6764362e92e2c33e57
Bartkuip <99027534@mydavinci.nl>
2022-11-29 12:53:54 +0100

    opdracht 11

---
+++ 04_lijstjes_en_samenstellingen/fruitmand/opdracht11.py
@@ -0,0 +1,32 @@
+from fruitmand import fruitmand
+kleurenLijst = []
+for x in range(len(fruitmand)):
+    if fruitmand[x]['color'] not in kleurenLijst:
+        kleurenLijst.append((fruitmand[x]['color']))
+        
+        
+print(f"Je hebt keuze uit de kleuren: {' '.join(kleurenLijst)}")
+kleurInput = input("Welke kleur wil je gebruiken? ").lower()
+
+
+rond, nietRond, welRond = 0, 0, 0
+
+for x in range(len(fruitmand)):
+    kleurLijst = fruitmand[x].get("color")
+    if kleurLijst == kleurInput:
+        if fruitmand[x].get("round"):
+            rond += 1
+            welRond += 1
+        else:
+            rond -= 1
+            nietRond += 1
+
+print(f"{rond} rond, {nietRond} niet rond, {welRond} wel rond")
+
+#“Er zijn {X} meer ronde vruchten dan niet ronde vruchten in de kleur {kleur}”
+if welRond > nietRond:
+    print(f"Er zijn {rond} meer ronde vruchten dan niet ronde vruchten in de kleur{kleurInput}")
+elif nietRond > welRond:
+    print(f"Er zijn {rond} minder ronde vruchten dan ronde vruchten in de kleur '{kleurInput}'")
+else:
+    print(f"Evenveel ronde vruchten als niet ronde vruchten")
