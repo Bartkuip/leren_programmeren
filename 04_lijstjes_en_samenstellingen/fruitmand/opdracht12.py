68ad28393db3626de728c324027f279446b87fb7
Bartkuip <99027534@mydavinci.nl>
2022-11-29 13:46:03 +0100

    opdracht 12

---
+++ 04_lijstjes_en_samenstellingen/fruitmand/opdracht12.py
@@ -0,0 +1,28 @@
+from fruitmand import fruitmand
+namenLijst = []
+lengte = 0
+for x in range(0, len(fruitmand)):
+    nameList = len(fruitmand[x].get("name"))
+    if nameList > lengte:
+        lengte = nameList
+
+for x in range(0, len(fruitmand)):
+    if  lengte == len(fruitmand[x].get("name")):
+        naam = fruitmand[x].get("name")
+        kleur = fruitmand[x].get("color")
+        gewicht = fruitmand[x].get("weight")
+        break
+
+kleurTranslation = {
+    "green" : "groene",
+    "red" : "rode",
+    "orange" : "oranje",
+    "brown" : "bruin",
+    "black" : "zwart",
+    "yellow" : "gele",
+    "blue" : "blauw",
+    "purple" : "paars",
+    "pink" : "roze"
+}
+
+print(f'De "{naam}" ({lengte} letters) heeft een {kleur} kleur en een gewicht van {gewicht} kg.')
