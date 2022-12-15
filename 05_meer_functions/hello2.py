af6896a5f4e3726aa71679aba17a347efbce4fe5
Bartkuip <99027534@mydavinci.nl>
2022-12-15 12:02:26 +0100

    hello from function town 2

---
+++ 05_meer_functions/hello2.py
@@ -0,0 +1,7 @@
+def halloWereld(fname):
+    end = ""
+    for x in range(1, fname + 1):
+        end += f"Hello from function town {x}\n!"
+    return end    
+userInput = int(input("Hoeveel?"))
+print(halloWereld(userInput))
