ece3e195e1b6a2d4f31b9001c4c470d26b1c3825
Bartkuip <99027534@mydavinci.nl>
2023-02-23 12:12:52 +0100

    power klaar

---
+++ challenges/power.py
@@ -0,0 +1,7 @@
+getal1 = int(input("Getal 1?: "))
+getal2 = int(input("Getal 2?: "))
+def powerCalc(input1, input2):
+    total = pow(input1, input2)
+    return total
+total = powerCalc(getal1, getal2)
+print(total)
